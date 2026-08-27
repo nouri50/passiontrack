080c81d6c46a22611e990fe70d5eb0fb17c331f4
nouri50 <n.morouche@gmail.com>
2026-08-27 23:13:05 +0200

    Auth: refresh token + route protégée /api/me fonctionnels

--- backend/src/Entity/User.php
+++ backend/src/Entity/User.php
@@ -6,10 +6,12 @@ use App\Repository\UserRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 #[ORM\Table(name: '`user`')]
-class User
+class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
@@ -94,7 +96,7 @@ class User
         return $this;
     }
 
-    public function getUsername(): ?string
+    public function getUsernameField(): ?string
     {
         return $this->username;
     }
@@ -184,13 +186,30 @@ class User
 
     public function getRoles(): array
     {
-        return $this->roles;
+        $roles = $this->roles;
+        $roles[] = 'ROLE_USER';
+        return array_unique($roles);
     }
 
     public function setRoles(array $roles): static
     {
         $this->roles = $roles;
         return $this;
+    }
+
+    public function getPassword(): string
+    {
+        return $this->password_hash;
+    }
+
+    public function eraseCredentials(): void
+    {
+        // Rien à effacer
+    }
+
+    public function getUserIdentifier(): string
+    {
+        return $this->email;
     }
 
     public function getCreatedAt(): ?\DateTimeImmutable
